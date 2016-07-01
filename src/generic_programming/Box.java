5a9ecce0dc25b4b1a941e1bce604184d3773028d
Marc Werenfels <marc.werenfels@gmail.com>
2016-07-01 09:19:53 +0200

    generic programming

---
+++ src/generic_programming/Box.java
@@ -0,0 +1,30 @@
+package generic_programming;
+
+public class Box<T> {
+
+	private T content;
+
+	public Box(T content) {
+		this.content = content;
+	}
+
+	public void setContent(T content) {
+		this.content = content;
+	}
+
+	public T getContent() {
+		return this.content;
+	}
+
+	public static void main(String[] args) {
+
+		Box<String> stringBox = new Box<>("HelloWorld");
+		String s = stringBox.getContent();
+
+		Box<Integer> integerBox = new Box<>(100);
+		int i = integerBox.getContent();
+
+		Box<Box<Integer>> doubleIntegerBox = new Box<>(integerBox);
+		int j = doubleIntegerBox.getContent().getContent();
+	}
+}
---
+++ src/generic_programming/PairOfBoxes.java
@@ -0,0 +1,45 @@
+package generic_programming;
+
+import java.util.List;
+
+public class PairOfBoxes<S, T> extends Pair<Box<S>, Box<T>> {
+
+	public PairOfBoxes(Box<S> first, Box<T> second) {
+		super(first, second);
+	}
+
+	public PairOfBoxes(S firstContent, T secondContent) {
+		super(new Box<S>(firstContent), new Box<T>(secondContent));
+	}
+
+	public S getFirstContent() {
+		return this.getFirst().getContent();
+	}
+
+	public T getSecondContent() {
+		return this.getSecond().getContent();
+	}
+
+	public void setFirstContent(S content) {
+		this.getFirst().setContent(content);
+	}
+
+	public void setSecondContent(T content) {
+		this.getSecond().setContent(content);
+	}
+	
+	public static void main(String[] args) {
+
+		PairOfBoxes<String, Integer> pairOfBoxes = new PairOfBoxes<>("HelloWorld", 200);
+
+		Box<String> box1 = pairOfBoxes.getFirst();
+        System.out.println(box1.getContent());
+        Box<Integer> box2 = pairOfBoxes.getSecond();
+        System.out.println(box2.getContent());
+
+		String s = pairOfBoxes.getFirstContent();
+        System.out.println(s);
+        Integer i = pairOfBoxes.getSecondContent();
+        System.out.println(i);
+    }
+}
