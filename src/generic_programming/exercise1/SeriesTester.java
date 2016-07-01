cb2881645d0c814cdf7f30767a619f97715f2e3a
Christian Colic <colic.christian@gmail.com>
2016-07-01 09:52:13 +0200

    Fix SeriesTester

--- src/generic_programming/exercise1/SeriesTester.java
+++ src/generic_programming/exercise1/SeriesTester.java
@@ -4,11 +4,11 @@ public class SeriesTester {
 
 	public static void main(String[] args) {
 
-		Series<Integer> s1 = new Series<>(0, x -> x + 1, x -> x <= 10);
-		Series<Integer> s2 = new Series<>(0, x -> x + 2, 10);
-		Series<Integer> s3 = new Series<>(1, x -> x + x, x -> x <= 80);
-		Series<String> s4 = new Series<>("", s -> s + "*", s -> s.length() <= 10);
-		Series<String> s5 = new Series<>("HelloWorld", s -> s.substring(0, s.length() - 1), s -> !s.equals(""));
+		Series<Integer> s1 = new Series<Integer>(0, x -> x + 1, x -> x <= 10);
+		Series<Integer> s2 = new Series<Integer>(0, x -> x + 2, 10);
+		Series<Integer> s3 = new Series<Integer>(1, x -> x + x, x -> x <= 80);
+		Series<String> s4 = new Series<String>("", s -> s + "*", s -> s.length() <= 10);
+		Series<String> s5 = new Series<String>("HelloWorld", s -> s.substring(0, s.length() - 1), s -> !s.equals(""));
 
 		while (s1.hasNext()) {
 			System.out.print(s1.next() + " ");
