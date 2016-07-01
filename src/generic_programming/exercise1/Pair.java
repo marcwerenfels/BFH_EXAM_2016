7673caef50e9f59629cc7ec5223971a930ca62a7
Christian Colic <colic.christian@gmail.com>
2016-07-01 09:57:19 +0200

    remove some shit

---
+++ src/generic_programming/exercise1/Pair.java
@@ -0,0 +1,39 @@
+package generic_programming.exercise1;
+
+public class Pair<S, T> {
+
+	private S first;
+	private T second;
+
+	public Pair(S first, T second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public S getFirst() {
+		return this.first;
+	}
+
+	public T getSecond() {
+		return this.second;
+	}
+
+	public void setFirst(S first) {
+		this.first = first;
+	}
+
+	public void setSecond(T second) {
+		this.second = second;
+	}
+
+	public static void main(String[] args) {
+
+		Pair<String, Integer> pair1 = new Pair<>("HelloWorld", 100);
+		String s1 = pair1.getFirst();
+		int i1 = pair1.getSecond();
+
+		Pair<Integer, String> pair2 = new Pair<>(100, "HelloWorld");
+		int i2 = pair2.getFirst();
+		String s2 = pair2.getSecond();
+	}
+}
